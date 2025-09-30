02ff9d2dabd1c73b46112e1be327c7bff3711eb5
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-30 15:31:07 -0500

    split example update commit

---
+++ src/main/java/com/pluralsight/SplitExample.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.util.regex.Pattern;
+
+public class SplitExample {
+    public static void main(String[] args) {
+
+        String input = "Dallas|Ft. Worth|Austin";
+        String[] cities = input.split(Pattern.quote("|"));
+
+//      cities is an array containing 3 strings
+//      [0] is Dallas, [1] is Ft. Worth, [2] is Auston
+
+        System.out.println(cities[1]);
+
+    }
+}
