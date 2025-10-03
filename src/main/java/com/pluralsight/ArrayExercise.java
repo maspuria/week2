ce67ceef87c3e11bf78f89f4a5214fc49f69baff
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-03 12:53:56 -0500

    array exercise examples

---
+++ src/main/java/com/pluralsight/ArrayExercise.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class ArrayExercise {
+    public static void main(String[] args) {
+//        Exercise 1
+
+        int[] numbers = {11,22,33,44,55};
+
+        int luckyNum1 = numbers[0];
+        System.out.println(luckyNum1);
+
+        int luckyNum2 = numbers[1];
+        System.out.println(luckyNum2);
+
+        System.out.println(numbers[4]);
+
+//      Exercise 2
+
+        String[] artists = {"Lady Gaga","Justin Bieber","Twice","Keshi","Alleycvt"};
+
+        String fav1 = artists[0];
+        System.out.println(fav1);
+
+        System.out.println(artists[3]);
+
+    }
+
+
+}
