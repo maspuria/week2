df1ed8633685d986ccbcbcbdae5c396f6173d31a
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-03 12:53:33 -0500

    for loop commit

---
+++ src/main/java/com/pluralsight/forLoopExample.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class forLoopExample {
+
+    public static void main(String[] args) {
+       // counter
+//       int count = 1;
+//        //condition
+//        while ( count <= 5){
+//        System.out.println(count);
+//        count = count + 1; //increment OR count++
+//        }
+
+        for (int count = 1; count <=5;count = count + 1){
+            System.out.println(count);
+        }
+//        harder to read, developereze version
+        for (int i = 0; i <=5; i++){
+            System.out.println(i);
+        }
+
+
+
+    }
+
+
+}
