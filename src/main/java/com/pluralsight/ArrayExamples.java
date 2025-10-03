4ef45bf4c24875289d35eea8d222b5811794ac77
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-03 12:53:47 -0500

    array examples

---
+++ src/main/java/com/pluralsight/ArrayExamples.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class ArrayExamples {
+
+    public static void main(String[] args) {
+        int[] numbers = {54,77,88,99,12,83};
+//        System.out.println("pause");
+
+        int numberA = numbers[4]; // number[4] is a subscript
+        System.out.println(numberA);
+
+        int numberB = numbers[2];
+        System.out.println(numberB);
+
+        System.out.println(numbers[5]);
+
+//        for (initialization;condition;increment) {
+//        }
+//        for (int count = 0;condition;increment) {
+//        }
+
+
+    }
+
+}
